8875106ef942c232bb5ee4a36e7faa895967b1b1
Mubarak T H A N  Aloraifan <83314@kfh.com>
2024-01-02 15:45:02 +0300

    Task-Arimeticks

---
+++ src/Arithmeticstask.java
@@ -0,0 +1,13 @@
+public class Arithmeticstask {
+    public static void main(String[] args) throws Exception {
+        double amountKWD = 86;
+        System.out.println("Kuwaiti dinar to US Dollars ");
+        System.out.println(amountKWD + " KWD" + " = "+ (amountKWD*3.25) + "USD");
+        double amountUSD = 279.5;
+        System.out.println("US Dollars to Kuwaiti dinar ");
+        System.out.println(amountUSD + " USD" + " = "+ (amountUSD/3.25) + "KWD");
+        double amountEUR = 255.42000000000002;
+        System.out.println("Euro to Kuwaiti dinar ");
+        System.out.println(amountEUR + " EUR" + " = "+ (amountEUR/2.97) + "KWD");
+    }
+}
